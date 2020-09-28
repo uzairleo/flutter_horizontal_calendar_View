75a4e61b16f128f650e594bafbf190f11b31b6d4
uzairleo <uzair.jan336@gmail.com>
2020-09-29 00:29:09 +0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/horizontal_calendar_leo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.horizontal_calendar_leo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,117 @@
+import 'package:date_picker_timeline/date_picker_timeline.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Leo Date Picker Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  var formattedTime;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  DatePickerController _controller = DatePickerController();
+
+  DateTime _selectedValue = DateTime.now();
+
+  String formatedTime;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      formatedTime = getMonth(_selectedValue);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.refresh),
+          onPressed: () {
+            _controller.animateToSelection();
+          },
+        ),
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(20.0),
+          color: Colors.blueGrey[100],
+          child: Stack(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "${this.formatedTime}",
+                    style:
+                        TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(20),
+                  ),
+                  Container(
+                    child: DatePicker(
+                      DateTime.now(),
+                      width: 60,
+                      height: 80,
+                      controller: _controller,
+                      initialSelectedDate: DateTime.now(),
+                      selectionColor: Colors.transparent,
+                      selectedTextColor: Colors.black,
+                      locale: "en_us",
+                      monthTextStyle: TextStyle(color: Colors.transparent),
+                      onDateChange: (date) {
+                        // New date selected
+                        setState(() {
+                          _selectedValue = date;
+
+                          formatedTime = getMonth(_selectedValue);
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              Positioned(
+                left: 130,
+                top: 220,
+                child: Opacity(
+                  opacity: 0.6,
+                  child: Container(
+                    width: 70,
+                    height: 100,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12.0),
+                        color: Colors.blue.withOpacity(0.6)),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+
+  getMonth(value) {
+    return DateFormat.MMMM().format(value);
+  }
+}
